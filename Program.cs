17177b54afe9966c5c20d34a819986ca2b2e47e4
WhatIsFun <mohammed110051@gmail.com>
2023-07-07 19:44:38 +0400

    Task 1 Read and Write from the file

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿namespace cSharp_Filing
+﻿using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace cSharp_Filing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string exampleFileWithUsing = "Example File with Using.txt";
-            WriteFile(exampleFileWithUsing);
+            //WriteFile(exampleFileWithUsing);
+            //Console.WriteLine("Reading from the file");
+
+
+            // Task 1 
+            string fileName = "Task 1.doxc";
+            string content = "Task 1 content";
+            Task1(fileName, content);
+            
         }
         private static void WriteFile(string exampleFile)
         {
+            
             StreamWriter writer = null;
             try
             {
@@ -22,6 +35,37 @@
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+            }
+        }
+
+        // Task 1:Write a program:
+        // That writes the content to a file.
+        // That can read contents of a file.
+        // If the file exist, it appends/adds the text to the file.
+        private static void Task1(string fileName, string content)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                sw.WriteLine(content);
+            }
+            //Console.WriteLine("The file is Created");
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Reading from the {fileName}");
+                    Console.WriteLine(line);
+                }
+            }
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine("File is Created");
+            }
+            else
+            {
+                Console.WriteLine("File is already exist");
             }
         }
     }
