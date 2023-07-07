c46f720b0db8076928e23c6adad489a92004a083
WhatIsFun <mohammed110051@gmail.com>
2023-07-07 13:48:22 +0400

    cSharp Filing

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace cSharp_Filing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exampleFileWithUsing = "Example File with Using.txt";
+            WriteFile(exampleFileWithUsing);
+        }
+        private static void WriteFile(string exampleFile)
+        {
+            StreamWriter writer = null;
+            try
+            {
+                using (writer = new StreamWriter(exampleFile))
+                {
+                    writer.WriteLine("TEST with using keyword");
+                }
+                Console.WriteLine("File writing complete");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
